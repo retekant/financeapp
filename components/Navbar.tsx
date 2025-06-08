1e9e1c896aa38bbd5873d3bd5eb67110ffac1ab8
retekant <retekant@gmail.com>
2025-06-07 22:49:18 -0700

    fixed the navbar to make it look better

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,32 +14,29 @@ export default function Navbar() {
   };
 
   return (
-    <div className={`absolute right-0 h-full w-1/6 backdrop-blur-sm z-10  bg-gray-500/30
-    ${isMinimized ? 'w-16' : 'w-1/6'} transition-all duration-300`}>
+    <div className={`absolute right-0 h-full backdrop-blur-sm z-10 bg-gray-500/30
+    ${isMinimized ? 'w-18' : 'w-1/6'} transition-all duration-500 ease-in-out`}>
       <button 
         onClick={toggleMinimize} 
-        className={`absolute top-2 left-5  bg-gray-800 hover:bg-gray-500
-         text-white rounded-md z-20 ${isMinimized ? 'px-2' : 'px-[6.4rem]'}
-         transition-all duration-300`}
+        className={`absolute top-2 ${isMinimized ? 'left-2' : 'left-5'} bg-gray-800 hover:bg-gray-500
+         text-white rounded-md z-20 ${isMinimized ? 'px-2' : 'px-4 w-[80%]'}
+         transition-all duration-500 ease-in-out`}
       >
-        X
+        {isMinimized ? '→' : '←'}
       </button>
         
-      {!isMinimized && (
-        <div className='flex flex-col gap-5 text-xl font-semibold 
-        mt-12 items-center w-full'>
+      <div className={`flex flex-col gap-5 text-xl font-semibold 
+        mt-12 items-center w-full transition-all duration-500 ease-in-out
+        ${isMinimized ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}>
             
-        <Link href="/" className={`  hover:scale-125 
-            ${isMinimized ? 'opacity-0' : 'opacity-100'} transition-all duration-300`}>
-          <div> Home</div>
+        <Link href="/" className="hover:scale-125 transition-all duration-300">
+          <div>Home</div>
         </Link>
 
-        <Link href="/history" className={`  hover:scale-125 
-            ${isMinimized ? 'opacity-0' : 'opacity-100'} transition-all duration-300`}>
-        <div> History</div>
+        <Link href="/history" className="hover:scale-125 transition-all duration-300">
+          <div>History</div>
         </Link>
-        </div>
-        )}
+      </div>
     </div>
   );
 }
